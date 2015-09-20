91f3482884c5e88ea235843cb7fbd78159bf30b6
briandmpls <briandmpls@gmail.com>
2015-09-20 12:11:56 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,47 @@
+
+function searchCallback(results) {
+    console.log(results);
+}
+var jon ;
+$(document).ready(function() {
+	
+	
+	$('.submitButton').on("click",function(event){
+		event.preventDefault();
+		
+
+		var lookFor = $(".inputText").val();
+		alert(lookFor);
+		console.log(lookFor);
+		search(lookFor);
+	});
+});
+
+
+
+
+
+
+
+function search(query){
+	$.ajax ({
+	    type: 'GET',
+	    dataType: 'json',
+	    crossDomain: true,
+	    json: 'json_callback',
+	    url: "http://api.giphy.com/v1/gifs/search?q=" + query +"&api_key=dc6zaTOxFJmzC"  
+	}).done(function(response){
+		searchCallback(response);
+
+				
+		jon = "<img src =" + response.data[2].images.original.url+"></img>";
+		$(".placeHere").append(jon);
+		
+		
+
+		
+		console.log(response.data.length);
+
+	});
+	
+};
